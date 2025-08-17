04c9f8e65f5b8baf1afa45ded0e857df09499377
ElviaEchauri <echaur.ely@gmail.com>
2025-08-17 15:51:28 -0600

    created proyect

---
+++ src/main/java/com/aluracursos/desafiolibros/DesafiolibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiolibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiolibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiolibrosApplication.class, args);
+	}
+
+}
